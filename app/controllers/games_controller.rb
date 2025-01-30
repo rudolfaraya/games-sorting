9e817c32809d050860c7fffdd22d2dad1d21c518
Rodolfo Araya <rudolfaray2@gmail.com>
2025-01-30 20:31:21 -0300

    Refactor y filtros por plataforma y género

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,8 +7,10 @@ class GamesController < ApplicationController
     session["filters"].merge!(filters_params)
 
     @games = Game.includes(:genre, :platform)
-    @games = @games.where("games.title like ?", "%#{session["filters"]["title"]}%") if session["filters"]["title"].present?
-    @games = @games.order(session["filters"].slice("column", "direction").values.join(" "))
+                  .then { search_by_title _1 }
+                  .then { filter_by_genre _1 }
+                  .then { filter_by_platform _1 }
+                  .then { apply_order _1 }
   end
 
   # GET /games/1 or /games/1.json
@@ -74,6 +76,22 @@ class GamesController < ApplicationController
     end
 
     def filters_params
-      params.permit(:title, :column, :direction)
+      params.permit(:title, :genre_id, :platform_id, :column, :direction)
+    end
+
+    def search_by_title(scope)
+      session['filters']['title'].present? ? scope.where('games.title like ?', "%#{session['filters']['title']}%") : scope
+    end
+
+    def filter_by_genre(scope)
+      session['filters']['genre_id'].present? ? scope.where(genre_id: session['filters']['genre_id']) : scope
+    end
+
+    def filter_by_platform(scope)
+      session['filters']['platform_id'].present? ? scope.where(platform_id: session['filters']['platform_id']) : scope
+    end
+
+    def apply_order(scope)
+      scope.order(session['filters'].slice('column', 'direction').values.join(' '))
     end
 end
